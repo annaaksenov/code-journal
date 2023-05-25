79f2885be4fa3bd09aa9e2cd453d8de7c9b5b220
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-25 23:09:45 +0000

    started on submit function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const form = document.querySelector('form');
 const photoURL = document.querySelector('.photo-url');
 const previewPhoto = document.querySelector('img');
-
+photoURL.addEventListener('input', handleInput);
 function handleInput(event) {
-  const obj = {
+  const urlObj = {
     $url: form.elements.url.value
   };
-  previewPhoto.setAttribute('src', obj.$url);
+  previewPhoto.setAttribute('src', urlObj.$url);
 }
-photoURL.addEventListener('input', handleInput);
+
+const sub = document.querySelector('.submit');
+sub.addEventListener('click', handleClick);
+function handleClick(event) {
+  event.preventDefault();
+}
+/**
+ * const submitObj = {
+ * $title: form.elements.title.value,
+ * $url: form.elements.url.value,
+ * $notes: form.elements.notes.value
+ * };
+ * */
