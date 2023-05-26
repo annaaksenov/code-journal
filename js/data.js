51cc5adce6e73cdaf8ac19625c063949ec17087a
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-26 04:07:25 +0000

    Added beforeunload to window

--- js/data.js
+++ js/data.js
@@ -6,3 +6,9 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', handleStorage);
+function handleStorage(event) {
+  const serialize = JSON.stringify(data);
+  localStorage.setItem('local-storage', serialize);
+}
