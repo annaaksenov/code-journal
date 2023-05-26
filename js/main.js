bbab69fdc2a4e9db1fddd3464a16cfb6e31b8f11
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-26 03:34:41 +0000

    Added a submit event listener function

--- js/main.js
+++ js/main.js
@@ -13,11 +13,16 @@ const sub = document.querySelector('.submit');
 sub.addEventListener('click', handleClick);
 function handleClick(event) {
   event.preventDefault();
+  const submitObj = {
+    $title: form.elements.title.value,
+    $url: form.elements.url.value,
+    $notes: form.elements.notes.value
+  };
+  const nextId = data.nextEntryId;
+  submitObj.entryId = nextId;
+  data.nextEntryId++;
+  data.entries.unshift(submitObj);
+  // may be reason for future hiccups ^
+  previewPhoto.setAttribute('src', '/images/placeholder-image-square.jpg');
+  form.reset();
 }
-/**
- * const submitObj = {
- * $title: form.elements.title.value,
- * $url: form.elements.url.value,
- * $notes: form.elements.notes.value
- * };
- * */
