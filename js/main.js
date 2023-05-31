d35bd2558ca4595527b6de63c4c5f2ee7b298933
Anna Aksenov <annaaksenov8@gmail.com>
2023-05-31 05:01:20 +0000

    Defined a function that returns a DOM tree

--- js/main.js
+++ js/main.js
@@ -23,3 +23,28 @@ function handleClick(event) {
   previewPhoto.setAttribute('src', '/images/placeholder-image-square.jpg');
   form.reset();
 }
+
+function renderEntry(entry) {
+  // const ul = document.getElementById('#ulist');
+  const li = document.createElement('li');
+  const row = document.createElement('div');
+  row.className = 'row';
+  const colHalf = document.createElement('div');
+  colHalf.className = 'column-half';
+  const img = document.createElement('img');
+  img.setAttribute('src', entry.photoURL);
+  // ^ Could be data.entries.photoURL OR entry.$url
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+
+  // ul.appendChild(li);
+  li.appendChild(row);
+  row.appendChild(colHalf);
+  colHalf.appendChild(img);
+  img.appendChild(h3);
+  img.appendChild(p);
+  // return ul;
+  return li;
+}
+// continue here
+if (renderEntry);
